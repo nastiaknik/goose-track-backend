113c71532891c28089f04da921933d727c4114db
nastiaknik <anastazja.knihnitska@gmail.com>
2023-06-26 11:09:44 +0200

    remove versionKey and add timestamps to task model

--- models/task.js
+++ models/task.js
@@ -1,28 +1,31 @@
 const { model, Schema } = require("mongoose");
 
-const taskSchema = new Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required"],
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Low",
+    },
+    start: String,
+    end: String,
+    date: String,
+    category: {
+      type: String,
+      enum: ["To do", "In progress", "Done"],
+      default: "To do",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
   },
-  priority: {
-    type: String,
-    enum: ["Low", "Medium", "High"],
-    default: "Low",
-  },
-  start: String,
-  end: String,
-  date: String,
-  category: {
-    type: String,
-    enum: ["To do", "In progress", "Done"],
-    default: "To do",
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: "user",
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const Task = model("task", taskSchema);
 
