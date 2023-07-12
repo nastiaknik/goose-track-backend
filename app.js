3ac43e6d2221e172b698fbecaa0e035b03c1d023
Milosska <110258611+Milosska@users.noreply.github.com>
2023-07-12 19:57:31 +0300

    adds cookieParser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDoc = require("./swagger.json");
 
@@ -17,6 +18,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.static("public"));
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
